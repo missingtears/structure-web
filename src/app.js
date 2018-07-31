a8d361bccb68d9b99d271a6b4aa8b577c624e378
missingtears <f.ruguo@gmail.com>
2018-08-01 06:30:30 +0800

    add demo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import ReactDOMServer from 'react-dom/server';
+import views from 'koa-views';
+import App from './demo';
 
 const port = 3000;
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-    ctx.body = "home";
+app.use(views(__dirname + '/views', {
+    map: {
+        html: 'ejs'
+    }
+}));
+
+router.get('/', async (ctx, next) => {
+    const content = ReactDOMServer.renderToString(<App />);
+    await ctx.render('demo', {
+        html: content  // 将 html 字符串通过 ejs 模板渲染出来
+    });
 });
 
 router.get('/create', (ctx, next) => {
